f560ec87190d5f372b6e977d36769480d3865554
Dmytro2V <op2vin@gmail.com>
2023-03-23 18:53:03 +0200

    done

--- src/index.js
+++ src/index.js
@@ -2,10 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, useParams } from 'react-router-dom';
+import Users from './components/Users';
+import Profile from './components/Profile';
 
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        {/* TODO: Routes */}
+        <Route exact path="/">
+          <App />    
+             
+        </Route>
+
+        <Route path="/users">
+          <div>
+            <Users />          
+          </div>
+        </Route>
+
+        <Route path="/users/:userId">
+          <div>
+            <Profile />          
+          </div>
+        </Route>
+      </div>
+    </BrowserRouter>
+  );
+};
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
